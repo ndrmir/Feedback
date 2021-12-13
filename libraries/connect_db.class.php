68c3088ed3b78613245e5729fb565c36a44f3d83
ndrmir <90983989+ndrmir@users.noreply.github.com>
2021-12-13 21:51:34 +0300

    Add files via upload

---
+++ libraries/connect_db.class.php
@@ -0,0 +1,44 @@
+<?php
+
+//Класс для работы с базой данных
+
+Class Connect_db {
+
+    protected $dbname = 'feedback';
+    protected $dbhost = 'localhost';
+    protected $dbusername = 'root';
+    protected $dbuserpassword = '';
+    protected $db;
+
+    public function __construct($dbhost, $dbusername, $dbuserpassword, $dbname) {
+
+        if($dbhost && $dbusername && $dbuserpassword && $dbname) {
+            $this->dbhost = $dbhost;
+            $this->dbusername = $dbusername;
+            $this->dbuserpassword = $dbuserpassword;
+            $this->dbname = $dbname;
+        }
+    }
+    
+    public function db_connect() {
+        $config = [
+            'dns' => 'mysql:host='.$this->dbhost.';dbname='.$this->dbname.';charset=utf8',
+            'username' => $this->dbusername,
+            'password' => $this->dbuserpassword,
+        ];
+        try {
+            
+            $this->db = new PDO($config['dns'], $config['username'], $config['password']);
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+            return $this->db;
+        } catch (PDOException $PDOException) {
+            echo $PDOException->getMessage();
+        }
+        return false;
+    }
+    public function db_close() {
+        $this->db = null;
+    }
+}
+
+?>
