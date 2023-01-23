d729ddccb4dbf3172487029ad95fd5a122a81016
Andrey <agallardo@yandex.ru>
2023-01-23 16:51:49 +0300

    edit with namespace, fix construct error

--- libraries/connect_db.class.php
+++ libraries/connect_db.class.php
@@ -1,44 +1,48 @@
 <?php
 
+namespace DB;
+
+use PDO;
+use PDOException;
+
 //Класс для работы с базой данных
-
-Class Connect_db {
-
+class ConnectDb
+{
     protected $dbname = 'feedback';
     protected $dbhost = 'localhost';
     protected $dbusername = 'root';
-    protected $dbuserpassword = '';
+    protected $dbuserpassword = 'root';
     protected $db;
 
-    public function __construct($dbhost, $dbusername, $dbuserpassword, $dbname) {
+    public function __construct($dbhost = null, $dbusername = null, $dbuserpassword = null, $dbname = null)
+    {
 
-        if($dbhost && $dbusername && $dbuserpassword && $dbname) {
+        if ($dbhost && $dbusername && $dbuserpassword && $dbname) {
             $this->dbhost = $dbhost;
             $this->dbusername = $dbusername;
             $this->dbuserpassword = $dbuserpassword;
             $this->dbname = $dbname;
         }
     }
-    
-    public function db_connect() {
+
+    public function connect()
+    {
         $config = [
-            'dns' => 'mysql:host='.$this->dbhost.';dbname='.$this->dbname.';charset=utf8',
+            'dns' => 'mysql:host=' . $this->dbhost . ';dbname=' . $this->dbname . ';charset=utf8',
             'username' => $this->dbusername,
             'password' => $this->dbuserpassword,
         ];
         try {
-            
             $this->db = new PDO($config['dns'], $config['username'], $config['password']);
-            $this->db->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             return $this->db;
         } catch (PDOException $PDOException) {
             echo $PDOException->getMessage();
         }
         return false;
     }
-    public function db_close() {
+    public function close()
+    {
         $this->db = null;
     }
 }
-
-?>
--- scripts/feedback_createDbAllTables.php
+++ scripts/feedback_createDbAllTables.php
@@ -1,23 +1,27 @@
 <?php
 
-/* 
- * Создание базы данных и всех таблиц для работы сайта!!!
- * 
- * 
+ini_set('display_errors', 'On');
+include_once "../libraries/connect_db.class.php";
+use DB\ConnectDb;
+
+/*
+ * Создание базы данных и всех таблиц для работы сайта
+ *
+ *
  */
 $dbname = 'feedback';
 $dbhost = 'localhost';
 $dbusername = 'root';
-$dbuserpassword = '';
+$dbuserpassword = 'root';
 
-function db_connect($dbhost, $dbusername, $dbuserpassword) { 
+function db_connect($dbhost, $dbusername, $dbuserpassword)
+{
     $config = [
-        'dns' => 'mysql:host='.$dbhost.';charset=utf8',
+        'dns' => 'mysql:host=' . $dbhost . ';charset=utf8',
         'username' => $dbusername,
         'password' => $dbuserpassword,
     ];
     try {
-        
         $db = new PDO($config['dns'], $config['username'], $config['password']);
         $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $db;
@@ -26,18 +30,21 @@ function db_connect($dbhost, $dbusername, $dbuserpassword) {
     }
     return false;
 }
-function db_close($PDO) {
+function db_close($PDO)
+{
         $PDO = null;
 }
 
 $PDO = db_connect($dbhost, $dbusername, $dbuserpassword);
 
-if(!$PDO)die("Не удалось подключиться к хосту $dbhost");
+if (!$PDO) {
+    die("Не удалось подключиться к хосту $dbhost");
+}
 
-try{
-    $PDO->query('CREATE DATABASE IF NOT EXISTS `'.$dbname.'`');
+try {
+    $PDO->query('CREATE DATABASE IF NOT EXISTS `' . $dbname . '`');
 } catch (PDOException $exception) {
-    echo $exception->getMessage()."Ошибка создания базы данных".$dbname;
+    echo $exception->getMessage() . "Ошибка создания базы данных" . $dbname;
     exit;
 }
 
@@ -47,12 +54,9 @@ db_close($PDO);
 
 
 
-
-include_once "../libraries/connect_db.class.php";
-
-$ObjDb = new Connect_db();
-$PDO = $ObjDb->db_connect();
-if(!$PDO){
+$ObjDb = new ConnectDb();
+$PDO = $ObjDb->connect();
+if (!$PDO) {
     die('Ошибка подключения к базе данных');
 }
 
@@ -60,16 +64,16 @@ if(!$PDO){
 $user_tablename = 'users';
 
 $table_def = "user_id SMALLINT NOT NULL AUTO_INCREMENT,";
-$table_def.="name VARCHAR(20) BINARY NOT NULL,";
-$table_def.="phone VARCHAR(20) NOT NULL,";
-$table_def.="email VARCHAR(40) NOT NULL,";
-$table_def.="PRIMARY KEY (user_id),";
-$table_def.="UNIQUE (email)";
+$table_def .= "name VARCHAR(20) BINARY NOT NULL,";
+$table_def .= "phone VARCHAR(20) NOT NULL,";
+$table_def .= "email VARCHAR(40) NOT NULL,";
+$table_def .= "PRIMARY KEY (user_id),";
+$table_def .= "UNIQUE (email)";
 
-try{
-    $PDO->query("CREATE TABLE $user_tablename ($table_def) ENGINE=InnoDB;");    
+try {
+    $PDO->query("CREATE TABLE $user_tablename ($table_def) ENGINE=InnoDB;");
 } catch (PDOException $exception) {
-    echo $exception->getMessage()."Ошибка создания таблицы данных".$user_tablename;
+    echo $exception->getMessage() . "Ошибка создания таблицы данных " . $user_tablename;
     exit;
 }
 
@@ -81,19 +85,19 @@ $file_tablename = 'files';
 
 $table_def = "";
 $table_def = "file_id SMALLINT NOT NULL AUTO_INCREMENT,";
-$table_def.="user_id SMALLINT NOT NULL,";
-$table_def.="filename VARCHAR(50) BINARY NOT NULL,";
-$table_def.="accessdate TIMESTAMP,";
-$table_def.="PRIMARY KEY (file_id),";
-$table_def.="FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE";
+$table_def .= "user_id SMALLINT NOT NULL,";
+$table_def .= "filename VARCHAR(50) BINARY NOT NULL,";
+$table_def .= "accessdate TIMESTAMP,";
+$table_def .= "PRIMARY KEY (file_id),";
+$table_def .= "FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE";
 
-try{
+try {
     $PDO->query("CREATE TABLE $file_tablename ($table_def) ENGINE=InnoDB;");
 } catch (PDOException $exception) {
-    echo $exception->getMessage()."Ошибка создания таблицы данных".$file_tablename;
+    echo $exception->getMessage() . "Ошибка создания таблицы данных" . $file_tablename;
     exit;
 }
 
 echo "Таблица $file_tablename успешно создана.<br />";
 
-$ObjDb->db_close();
+$ObjDb->close();
